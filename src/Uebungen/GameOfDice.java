b8f7c7a94c1e90ac42b58de37bf199a883b356d1
jodemaggio <jonas.haselbauer@gmail.com>
2018-11-04 19:21:07 +0100

    GameOfDice

--- src/Uebungen/GameOfDice.java
+++ src/Uebungen/GameOfDice.java
@@ -2,8 +2,29 @@ package Uebungen;
 
 public class GameOfDice {
 
-    public static String game(String[] arr){
-        return null;
+    public static String game(String[] args) {
+        int len, d1, s1, d2, s2;
+        String[] ds;
+        //array für das addieren der zahlen und e als entscheidungstraeger
+        for (int i = 0; i < args.length; i++) {
+            String zaehler =len;
+            String zaehler += args[i];
         }
 
+
+        //wert von e als Aussgabe
+        int e;
+        if (e == -1) {
+            System.out.println("Illegal Input");
+        } else if (e == 0) {
+            System.out.println("Draw");
+        } else if (e == 1) {
+            System.out.println("Player 1 won");
+        } else if (e == 2) {
+            System.out.println("Player 2 won");
+        } else {
+            System.out.println("Game Error");
+        }
+        return e;
+    }
 }
