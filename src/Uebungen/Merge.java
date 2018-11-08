c3d73c1072c9941d26373cc6c0fa2a34d77924d3
jodemaggio <jonas.haselbauer@gmail.com>
2018-11-08 16:49:25 +0100

    GameOfDice

---
+++ src/Uebungen/Merge.java
@@ -0,0 +1,16 @@
+package Uebungen;
+
+import static Uebungen.Merge.merge;
+
+public class Merge {
+    public static int merge(long[] ns, int i, Merger m) {
+        m.merge();
+        int b;
+        if (i < 0){
+            return merge(ns[0] + ns[b],i,m);
+            b++;
+        }
+        else if (i > 0){
+
+    }
+}
--- src/Uebungen/Negafibonacci.java
+++ src/Uebungen/Negafibonacci.java
@@ -1,4 +1,45 @@
 package Uebungen;
 
 public class Negafibonacci {
+
+    static int negaFibo(int n, NegaFiboRecCheck nfrc) {
+
+        nfrc.nfrc();
+
+        if (n == 0 | n == 1) {
+            return 1;
+        }
+        else if (n > 1) {
+            return negaFibo(n-2, nfrc) + negaFibo(n-1, nfrc);
+        }
+//            int a = 0;
+//            int b = 1;
+//            int i = 2;
+//            while (i <= n) {
+//                int aa = b;
+//                int bb = a + b;
+//                a = aa;
+//                b = bb;
+//                i++;
+//            }
+//             return negaFibo(n-2) + negaFibo(n-1);
+//            return b;
+        else if(n < 1) {
+            double x =new double[(negaFibo(n - 2, nfrc) + negaFibo(n - 1, nfrc)];
+            double v = (Math.pow((-1), (n + 1)));
+            return (int) (x*v);
+        }
+
+
+
+
+
+        }
+    }
+
 }
+
+
+
+
+
